b433284e4c96160ff5993b74a4db528ecedd6052
Michael F <tehfreak@awesome39.com>
2014-04-13 16:52:51 +0300

    Extended building, added builder dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,22 @@
 var gulp= require('gulp')
+
+var gulpNgmin= require('gulp-ngmin')
+var gulpUglify= require('gulp-uglify')
+var gulpRename= require('gulp-rename')
+
 var gulpLess= require('gulp-less')
+
+
+
+gulp.task('scripts', function () {
+    return gulp.src(['source/aeMenu/scripts/*.js'])
+        .pipe(gulp.dest('release/scripts'))
+        .pipe(gulpNgmin())
+        .pipe(gulpUglify())
+        .pipe(gulpRename({suffix: '.min'}))
+        .pipe(gulp.dest('release/scripts'))
+    ;
+})
 
 gulp.task('styles', function () {
     return gulp.src(['source/aeMenu/styles/*.less'])
@@ -8,8 +25,13 @@ gulp.task('styles', function () {
     ;
 })
 
+
+
 gulp.task('watch', function () {
+    gulp.watch(['source/aeMenu/scripts/**/*.js'], ['scripts'])
     gulp.watch(['source/aeMenu/styles/**/*.less'], ['styles'])
 })
 
-gulp.task('default', ['styles','watch'])
+
+
+gulp.task('default', ['scripts', 'styles','watch'])
